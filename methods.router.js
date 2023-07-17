4fd699819975ebdb09ee9c760a8578531df0630a
sulderyav <115753885+sulderyav@users.noreply.github.com>
2023-07-16 22:13:19 -0500

    methods.router.js

--- methods.router.js
+++ methods.router.js
@@ -1,16 +1,42 @@
 const express = require('express');
-
+const methodsService = require("../services/methods.services");
 const router = express.Router();
 
-router.get('/', (req, res) =>{
-  res.json({
-    method_id: 1234,
-    get: "GetValueToPay",
-    toString: "ToString"
+router.get("/", (req, res) => {
+  const allMethods = methodsService.find();
+  res.json(allMethods);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const method = methodsService.findOne(id);
+  res.json(method);
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: "Create",
+    data: body
   });
 });
 
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: "Update",
+    data: body,
+    id,
+  });
+});
 
-
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: "Delete",
+    id,
+  });
+});
 
 module.exports = router;

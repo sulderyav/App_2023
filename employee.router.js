4fcd3a14bcdbcade1fea56b2894fab292d9d483b
sulderyav <115753885+sulderyav@users.noreply.github.com>
2023-07-16 22:11:52 -0500

    employee.router.js

--- employee.router.js
+++ employee.router.js
@@ -1,16 +1,43 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/', (req, res) =>{
-  res.json({
-    method_id: 1234,
-    get: "GetValueToPay",
-    toString: "ToString"
+const employeeService = require("../services/employee.service");
+
+router.get("/", (req, res) => {
+  const allEmployees = employeeService.findAll();
+  res.json(allEmployees);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const employee = employeeService.findById(id);
+  res.json(employee);
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: "Create",
+    data: body
   });
 });
 
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: "Update",
+    data: body,
+    id,
+  });
+});
 
-
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: "Delete",
+    id,
+  });
+});
 
 module.exports = router;

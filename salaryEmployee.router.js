cc92f6f4a8c92383c3980573dab69ce97ebb60cc
sulderyav <115753885+sulderyav@users.noreply.github.com>
2023-07-16 22:10:41 -0500

    salaryEmployee.router.js

--- salaryEmployee.router.js
+++ salaryEmployee.router.js
@@ -1,17 +1,42 @@
 const express = require('express');
-
+const salaryEmployeeService = require("../services/salaryemployee.services");
 const router = express.Router();
 
-router.get('/', (req, res) =>{
-  res.json({
-    method_id: 2345,
-    get: "GetValueToPay",
-    toString: "ToString",
-    validation: "ValidateSalary"
+router.get("/", (req, res) => {
+  const allSalaryEmployee = salaryEmployeeService.find();
+  res.json(allSalaryEmployee);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const salaryEmployee = salaryEmployeeService.findOne(id);
+  res.json(salaryEmployee);
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: "Create",
+    data: body
   });
 });
 
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: "Update",
+    data: body,
+    id,
+  });
+});
 
-
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: "Delete",
+    id,
+  });
+});
 
 module.exports = router;

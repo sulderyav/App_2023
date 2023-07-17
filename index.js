5ab557636fe9c2e84f35ea7184526ff7a5431d5c
sulderyav <115753885+sulderyav@users.noreply.github.com>
2023-07-16 22:15:10 -0500

    index.js

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require('express');
+const router = express.Router();
+
 const invoiceRouter = require("./invoice.router");
 const methodsRouter = require("./methods.router");
 const employeeRouter = require("./employee.router");
-const methodsRouter = require("./methods.router");
-const { ro } = require('@faker-js/faker');
+const salaryEmployeeRouter = require("./salaryEmployee.router");
 
-const apiRouter = (app) => {
-  const router = express.Router();
-app.use('/api/v1', router)
-    router.use('/invoice', invoiceRouter); 
-    router.use('/methods', methodsRouter);
-app.use('/api/v2', router)  
-    router.use('/employee', employeeRouter);
-    
-}
+router.use('/invoice', invoiceRouter);
+router.use('/methods', methodsRouter);
+router.use('/employee', employeeRouter);
+router.use('/salaryEmployee', salaryEmployeeRouter);
 
-
-
-module.exports = apiRouter;
+module.exports = router;

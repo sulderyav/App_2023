77cb21a8fbba5f8872f594abbe97bb10eeaa600c
sulderyav <115753885+sulderyav@users.noreply.github.com>
2023-08-13 11:37:43 -0500

    Update employee.services.js

--- services/employee.services.js
+++ services/employee.services.js
@@ -7,9 +7,9 @@ class EmployeeService {
   constructor() {
     this.employees = [];
     this.generate();
-  }
+    }
 
- async generate() {
+  async generate() {
     for (let i = 0; i < 100; i++) {
       this.employees.push({
         employee_id: i + 1,
@@ -21,12 +21,13 @@ class EmployeeService {
       });
     }
   }
+  
   async find(){
     const client = await conexion();
     const rta =  await client.query("select * from employee");
     return rta.rows;
   }
-  
+ 
   async findAll() {
     return this.employees;
   }
@@ -36,12 +37,17 @@ class EmployeeService {
   }
 
   async created(data){
-    const newEmployee ={
-      id: faker.string.uuid(),
-      ...data
+    console.log(data);
+    const client = await conexion();
+    const sql = `insert into employee
+                (nombre,apellido,email,role)
+                values ("${data.nombre}","${data.apellido}", "${data.email}", "${data.role}")`;
+    const rta = await client.query(sql);
+    if(rta.rowcount != 1){
+      throw Boom.badRequest("not found"); 
+
     }
-    this.employees.push(newEmployee);
-    return newEmployee;
+    return rta; 
   }
 
   async update(id,data){
@@ -55,16 +61,56 @@ class EmployeeService {
     return this.employees[indexEmployee];
     
   }
-async deleted(id){
+
+  async deleted(id){
     const client = await conexion();
     const sql = "delete from employee where id=${id}"
     const rta = await client.query(sql);
     return rta.rows ; 
   }
 
+  
 }
 
 
 module.exports = EmployeeService;
 
 
+
+
+
+// class employee{
+
+//     constructor(){
+//         this.employee = [];
+//         this.generate();
+//     }
+
+//         generate(){
+//             for (let i = 0; i< 100; i++) {
+//                 this.employee.push(
+//                   {
+//                     employee_id: i + 1,
+//                     bornDate: 2002,
+//                     firstName: faker.person.firstName(),
+//                     hireDate: faker.date.hireDate(),
+//                     isActive: faker.status.isActive(),
+//                     lastName: faker.person.lastName()
+//                   }
+//                 )
+//               }
+//             }
+       
+
+//         find(){
+//             return this.employee;
+//         }
+
+//         findOne(id){
+//             return this.employee.find(em => em === id);
+//         }
+
+//     }
+
+
+//         module.exports = employee;
